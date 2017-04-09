4ad99eaf4120f530f4034f05b3ab817e04a2aa57
kaineko <chanodejs@yahoo.co.jp>
2017-04-10 02:19:28 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -48,67 +48,68 @@ window.onload = function () {
 		var ctx = canvas.getContext('2d');
 		ctx.strokeStyle = '#000';
 		ctx.lineWidth =6;
+		ctx.lineCap = 'round';
 		//色の変更
 		black.onclick = function(){
 			colorBox.style.display = 'none';
 			ctx.strokeStyle = '#000';
-		}
+		};
 		red.onclick = function(){
 			colorBox.style.display = 'none';
 			ctx.strokeStyle = '#F00';
-		}
+		};
 		green.onclick = function(){
 			colorBox.style.display = 'none';
 			ctx.strokeStyle = '#0F0';
-		}
+		};
 		blue.onclick = function(){
 			colorBox.style.display = 'none';
 			ctx.strokeStyle = '#00F';
-		}
+		};
 		yellow.onclick = function(){
 			colorBox.style.display = 'none';
 			ctx.strokeStyle = '#FF0';
-		}
+		};
 		aqua.onclick = function(){
 			colorBox.style.display = 'none';
 			ctx.strokeStyle = '#0FF';
-		}
+		};
 		purple.onclick = function(){
 			colorBox.style.display = 'none';
 			ctx.strokeStyle = '#F0F';
-		}
+		};
 		white.onclick = function(){
 			colorBox.style.display = 'none';
 			ctx.strokeStyle = '#FFF';
-		}
+		};
 		//太さの変更
 		superthin.onclick = function(){
 			boldBox.style.display = 'none';
 			ctx.lineWidth = 1;
-		}
+		};
 		thin.onclick = function(){
 			boldBox.style.display = 'none';
 			ctx.lineWidth = 3;
-		}
+		};
 		middle.onclick = function(){
 			boldBox.style.display = 'none';
 			ctx.lineWidth = 6;
-		}
+		};
 		thick.onclick = function(){
 			boldBox.style.display = 'none';
 			ctx.lineWidth = 9;
-		}
+		};
 		superthick.onclick = function(){
 			boldBox.style.display = 'none';
 			ctx.lineWidth = 12;
-		}
+		};
 		//全消の実装
 		eraseAll.onclick = function(){
 			if(window.confirm('全て消します。本当によろしいですか？')){	
 			ctx.fillStyle = '#FFF';
 			ctx.fillRect(0,0,w,h);
 			}
-		}
+		};
 		//ポインタの座標を取得
 		var offsetX;
 		var offsetY;
@@ -158,24 +159,53 @@ window.onload = function () {
 		canvas.onmousedown = function(e){
 			ctx.beginPath();
 			ctx.moveTo(offsetX,offsetY);
-		}
-		//ポインタが動いたら
+		};
+		//ポインタが動いたら描画
 		canvas.onmousemove = function(e){
 			if(e.buttons === 1){
 			ctx.lineTo(offsetX2,offsetY2);
+			lineToXLog.push(offsetX2);
+			lineToYLog.push(offsetY2);
 			ctx.stroke();
 			}
-		}
-		//左ボタンが離されたら描画終了
+		};
+		/*左ボタンが離されたらpathLogを作成し、sketchLogへ保存後削除する。
+		　また、lineToXLogとlineToYLogも初期化しておく。
+		*/
 		canvas.onmouseup = function(e){
-			ctx.lineTo(offsetX3,offsetY3);
-			ctx.stroke();
-		}
+			pathLog.push(ctx.strokeStyle,ctx.lineWidth,offsetX,offsetY,lineToXLog,lineToYLog);
+			sketchLog.push(pathLog);
+			pathLog = [];
+			lineToXLog =[];
+			lineToYLog =[];
+		};
 
 		//ポインタが画面外へ行った時の挙動
 		canvas.onmouseout = function (e){
-		 ctx.beginPath();	
+		 ctx.beginPath();
+		};
+		//戻るボタンの挙動
+		var sketchLog = [];//今までの全ての描写情報を保存する配列を用意
+		var pathLog = [];//1つのパスの間の描写情報を保存する配列を用意
+		var lineToXLog = [];//lineTo情報(X)を保存する配列を用意
+		var lineToYLog = [];//lineTo情報(Y)を保存する配列を用意
+		back.onclick = function(){
+			ctx.fillStyle = '#FFF';
+			ctx.fillRect(0,0,w,h);
+			sketchLog.pop();
+			for (var i = 0; i < sketchLog.length; i++){
+				ctx.beginPath();
+				ctx.strokeStyle = sketchLog[i][0];
+				ctx.lineWidth = sketchLog[i][1];
+				ctx.moveTo(sketchLog[i][2],sketchLog[i][3]);
+				for (var k = 0; k < sketchLog[i][4].length; k++){
+					ctx.lineTo(sketchLog[i][4][k],sketchLog[i][5][k]);
+				}
+				ctx.stroke();
+			}
 		}
+		
+		
 	}
 }
 		
