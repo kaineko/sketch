8d8e2d7fe3ba24a16fd09003b652890b63bd3e65
kaineko <chanodejs@yahoo.co.jp>
2017-04-13 04:36:37 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -400,8 +400,8 @@ window.onload = function () {
 					}
 				}
 		 	}
-				for(var fX = 1;fX < w; fX = fX+3){
-					for(var fY = 1; fY < h; fY = fY+3){
+				for(var fX = 1;fX < w; fX = fX+10){
+					for(var fY = 1; fY < h; fY = fY+10){
 						if ( ctx.isPointInPath(fX,fY)){
 							ctx.lineTo(fX,fY);
 						}
